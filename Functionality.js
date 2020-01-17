4570ca1b2780e7f2882aa217f23d4a792639f147
kashifAli96 <kashiftauseef@gmail.com>
2020-01-17 00:02:23 +0000

    splashcreen
    Navbar added
    splashscreen sorted
    j query used externally
    css handling the viewport for different screen sizes

--- Functionality.js
+++ Functionality.js
@@ -6,5 +6,19 @@ $(document).ready(function(){
   });
 
   // $("#game-container").click.
+//   $("#playbtn").click(function(){
+//     $("section").css("background-color", "black");
+//
+//
+// });
+
+  $("#playbtn").click(function(){
+    $("#p1").hide(1000);
+    $("#p2").hide(1500);
+    $("#p3").hide(2000);
+    $("#playbtn").hide();
+    // $("section").css("background-color", "white");
+
+});
 
 });
