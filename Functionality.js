ca4c9ad4d64b880a316c9638874d943ebeda9c9a
kashifAli96 <kashiftauseef@gmail.com>
2020-02-06 00:51:35 +0000

    Get player out of the loop
    pass the device need more algorithms
    
    use conter toggle for next player with loop

--- Functionality.js
+++ Functionality.js
@@ -1,6 +1,7 @@
 //ALL CANVAS CODE HERE
 $(document).ready(function(){
   var sp = " ";
+  var br = "<br>"; //use this for spacing
   var player1 = "";
   var player2 = "";
   var player3 = "";
@@ -11,10 +12,10 @@ $(document).ready(function(){
   var player8 = "";
 
   var players = [];
-
+  var word ="";
   // var player9 = "";
   // var player10 = "";
-  var br = "<br>"; //use this for spacing
+
   //var GameStart = null;
 
   var JSON = jQuery.parseJSON('{"Food":["greggs", "tea", "coffee","biscuits","Fish&Chips","Pasty","Meat Pie","Bagel","Sausage Roll","English Breakfast" ]}');
@@ -47,20 +48,6 @@ $(document).ready(function(){
   // ]
 
 
-
-
-  // var FOOD = {list: [
-  //     {name: "greggs"},
-  //     {name: "tea"},
-  //     {name: "coffee"},
-  //     {name: "biscuits"},
-  //     {name: "Fish&Chips"},
-  //     {name: "Pasty"},
-  //     {name: "Meat Pie"},
-  //     {name: "Bagel"},
-  //     {name: "Sausage Roll"},
-  //     {name: "English Breakfast"}]};
-
   // var LOCATIONS = {list: [
   // {name: "London"},
   // {name: "Manchester"},
@@ -75,7 +62,7 @@ $(document).ready(function(){
 
   $("#playerForm").hide(); // u can use a class to hide all context!
   $("#welcomeMsg").hide();
-  $(".hidden").hide()
+  $(".hidden").hide();
 
   // hide & show the navbar in mobile viewport only
   // $("#icon").click(function(){
@@ -89,7 +76,7 @@ $(document).ready(function(){
     $("#p3").hide();
     $("#playbtn").hide();
     $("#playerForm").show("slow");
-    $("section").css("background-color", "grey").show("slow");
+    // $("section").css("background-color", "grey").show("slow");
 
   });
 
@@ -126,6 +113,22 @@ $(document).ready(function(){
 
   });
 
+  var $toggle = -1;
+  $('#add').click(function(){
+     $toggle += 1;
+      if($toggle == 0){
+          $("#pl4").show();
+
+       }else if ($toggle == 1){$("#pl5").show();
+       }else if ($toggle == 2){$("#pl6").show();
+       }else if ($toggle == 3){$("#pl7").show();
+       }else if ($toggle == 4){$("#pl8").show();
+       // }else if ($toggle == 5){$("#pl9").show();
+       // }else if ($toggle == 6){$("#pl10").show();
+       }
+
+  });
+
 
   $("#startBtn").click(function(event){
     event.preventDefault();
@@ -133,43 +136,54 @@ $(document).ready(function(){
     var x="Display active player name here " + players.length;
     $("#checker").html(x);
 
-    // var activePlayers = 0;
-    // var i;
-    // for (i = 1; i < 8; i++){
-    // if (player[i] != ""){
-    //     activePlayers += 1;
-    //
-    //   }
-    //}
-
-
-
     //DISPLAY PLAYER NAMES FIRST
-    $("#ITEM").show();
-    var rnd = Math.floor(Math.random() * 10);
-    $("#ITEM").text(JSON.Food[rnd]); // USE RAND
-    $("#loadJson").show();
+    //$("#ITEM").show();
+    word = Math.floor(Math.random() * 10);
+    $("#ITEM").text(JSON.Food[word]); // USE RAND
+    $("#veryOdd").show();
+    $("#Start").show();
     // var myJSON = JSON.stringify(FOOD);//plain string not needed.
     // $("#ITEM").text(myJSON);
  });
 
 
 
- var $toggle = -1;
- $('#add').click(function(){
-    $toggle += 1;
-     if($toggle == 0){
-         $("#pl4").show();
 
-      }else if ($toggle == 1){$("#pl5").show();
-      }else if ($toggle == 2){$("#pl6").show();
-      }else if ($toggle == 3){$("#pl7").show();
-      }else if ($toggle == 4){$("#pl8").show();
-      // }else if ($toggle == 5){$("#pl9").show();
-      // }else if ($toggle == 6){$("#pl10").show();
-      }
+
+ //CONDITION TO GET 1 PLAYER OUT OF THE LOOP
+ $("#Start").click(function(event){
+    $("#Start").hide();
+   var OddManOut = players[Math.floor(Math.random() * players.length)];
+
+   $("#veryOdd").text("ODD MAN is " + OddManOut + " Word is  " + JSON.Food[word]).css("background-color", "red");
+
+
+   $('#PlayerSelection').text("Pass the device to " + players[0]);
+
+   $('#Confirmation').text("I confirm to be " + players[0]);
+   $("#Next").show();
+
+   $("#Next").click(function(){
+     $(".passandconfirm").hide();
+
+
+
+     if (players[0] != OddManOut){
+       $("#Question_Word").text("Here is the Word:");
+       $("#ITEM").show();
+     }
+     else if (players[0] == OddManOut) {
+       $('#PlayerSelection').text("You're out of the loop. Try to get the word from your opponent answers");
+     }
+   });
+ //}
+
 
  });
+
+
+
+
 
   // $("#loadJson").click(function(event){
   //                $.getJSON('foodQuestions.json', function() {
